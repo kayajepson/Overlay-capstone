08dda38847f47cf19025388bc7fc8e8704ccbd7a
Student <example@example.com>
2019-07-24 16:33:00 -0700

    upload test not working

--- src/components/Gallery.component.js
+++ src/components/Gallery.component.js
@@ -7,16 +7,54 @@ export default class Gallery extends React.Component {
     image: [],
   };
 
+  _pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+      base64: true
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      this.setState({
+        image: this.state.image.concat([result.uri])
+      })
+
+      let base64Img = `data:image/jpg;base64,${result.base64}`
+
+      //Add your cloud name
+      let apiUrl = 'https://api.cloudinary.com/v1_1/dzooqxmw2/image/upload';
+
+      let data = {
+        "file": base64Img,
+        "upload_preset": "ztu8dc7h",
+      }
+
+      fetch(apiUrl, {
+        body: JSON.stringify(data),
+        headers: {
+          'content-type': 'application/json'
+        },
+        method: 'POST',
+      }).then(async r => {
+        let data = await r.json()
+        console.log(data.secure_url)
+        return data.secure_url
+      }).catch(err=>console.log(err))
+    }
+  }
+
   render() {
     // let { image } = this.state;
 
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button
-          title="Pick an image from camera roll"
-          onPress={this._pickImage}
-        />
-        {this._renderImages()}
+      <Button
+      title="Pick an image from camera roll"
+      onPress={this._pickImage}
+      />
+      {this._renderImages()}
       </View>
     );
   }
@@ -26,27 +64,13 @@ export default class Gallery extends React.Component {
     this.state.image.map((item, index) => {
       images.push(
         <Image
-          key={index}
-          source={{ uri: item }}
-          style={{ width: 100, height: 100 }}
+        key={index}
+        source={{ uri: item }}
+        style={{ width: 100, height: 100 }}
         />
       );
     });
 
     return images;
   }
-  _pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      allowsEditing: true,
-      aspect: [4, 3],
-    });
-
-    console.log(result);
-
-    if (!result.cancelled) {
-      this.setState({
-        image: this.state.image.concat([result.uri]),
-      });
-    }
   };
-}
